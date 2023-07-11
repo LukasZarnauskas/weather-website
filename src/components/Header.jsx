98ec0f908fa7ad6e11257875c44ac3a0dec54331
GluteusMedius <zarnauskas97@gmail.com>
2023-07-11 16:23:52 +0300

    header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
+import WeatherButton from "./WeatherButton";
+
 function Header() {
+  const infoArray = ["Today", "Tomorrow", "Week"];
   return (
-    <div className="text-white">
-      <div>
-        <img className="" src="/src/images/Vector.png" alt="icon" />
-        <h1 className=" text-7xl">WeatherMe</h1>
-        <p className="text-xl">21:00pm</p>
+    <div className="text-white  flex  items-center">
+      <div className="flex flex-col">
+        <div className="flex">
+          <img
+            className="order-1  w-16 h-12"
+            src="/src/images/Vector.png"
+            alt="icon"
+          />
+          <h1 className=" order-2 mt-5 text-7xl">WeatherMe</h1>
+        </div>
+        <p className="text-right text-xl">21:00pm</p>
+      </div>
+      <div className="flex justify-around w-2/3">
+        {infoArray.map((day) => (
+          <WeatherButton key={day} day={day} />
+        ))}
       </div>
     </div>
   );
