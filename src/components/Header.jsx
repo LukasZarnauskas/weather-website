98b5d0f0f13a91cdbf4a5094e0e655fb91f399f4
GluteusMedius <zarnauskas97@gmail.com>
2023-07-12 12:28:58 +0300

    header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
-import WeatherButton from "./WeatherButton";
+import { useState } from "react";
 
 function Header() {
-  const infoArray = ["Today", "Tomorrow", "Week"];
+  const [active, setActive] = useState({
+    today: true,
+    tomorrow: false,
+    week: false,
+  });
+  function handleClick(e) {
+    if (e === "Today") {
+      setActive({ today: true, tomorrow: false, week: false });
+    } else if (e === "Tomorrow") {
+      setActive({ today: false, tomorrow: true, week: false });
+    } else if (e === "Week") {
+      setActive({ today: false, tomorrow: false, week: true });
+    }
+  }
   return (
     <div className="text-white  flex  items-center">
       <div className="flex flex-col">
@@ -16,9 +29,34 @@ function Header() {
         <p className="text-right text-xl">21:00pm</p>
       </div>
       <div className="flex justify-around w-2/3">
-        {infoArray.map((day) => (
-          <WeatherButton key={day} day={day} />
-        ))}
+        <button
+          onClick={() => handleClick("Today")}
+          className={` font-light text-3xl ${
+            active.today
+              ? "border-b-2 border-white text-white"
+              : "text-gray-300"
+          }`}
+        >
+          Today
+        </button>
+        <button
+          onClick={() => handleClick("Tomorrow")}
+          className={` font-light text-3xl ${
+            active.tomorrow
+              ? "border-b-2 border-white text-white"
+              : "text-gray-300"
+          }`}
+        >
+          Tomorrow
+        </button>
+        <button
+          onClick={() => handleClick("Week")}
+          className={` font-light text-3xl ${
+            active.week ? "border-b-2 text-white border-white" : "text-gray-300"
+          }`}
+        >
+          Week
+        </button>
       </div>
     </div>
   );
